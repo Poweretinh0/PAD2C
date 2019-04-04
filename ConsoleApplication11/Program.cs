d1da018ab4e78cb196eb04e035f03ed8ee2cb221
TekuIa <rcc.tiago@gmail.com>
2019-04-04 14:28:14 -0300

    Primeiro Commit

---
+++ ConsoleApplication11/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+
+    public class Empregados
+    {
+        public string PrimeiroNome;
+        public string Sobrenome;
+        public double Salario;
+
+        public string ReceberNome(){
+            return PrimeiroNome + Sobrenome;
+        }   
+
+        public double ReceberSalario(){
+            return Salario;
+        }
+
+        public void AumentarOSalario(){
+            Salario = Salario + Salario * 0.10;
+        }
+    }
+}
